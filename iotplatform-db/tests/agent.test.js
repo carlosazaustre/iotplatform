c4c607e617705162626782b1a6d74f3754523aa8
Carlos Azaustre <cazaustre@gmail.com>
2018-11-29 16:55:20 +0100

    define tests

---
+++ iotplatform-db/tests/agent.test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const test = require('ava')
+
+let config = {
+  logging: function () {}
+}
+
+let db = null
+
+test.beforeEach(async () => {
+  const setupDatabase = require('../')
+  db = await setupDatabase(config)
+})
+
+test('Agent', t => {
+  t.truthy(db.Agent, 'Agent service shoulld exist')
+})
