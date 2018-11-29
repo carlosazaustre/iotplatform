77347ea017f97219a6ac85deb5799ab1e384036c
Carlos Azaustre <cazaustre@gmail.com>
2018-11-29 17:45:15 +0100

    create stubs w/ Sinon

--- iotplatform-db/tests/agent.test.js
+++ iotplatform-db/tests/agent.test.js
@@ -1,18 +1,45 @@
 'use strict'
-
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
 const test = require('ava')
 
 let config = {
   logging: function () {}
 }
 
+let MetricStub = {
+  belongsTo: sinon.spy()
+}
+
+let AgentStub = null
 let db = null
+let sandbox = null
 
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.createSandbox()
+  AgentStub = {
+    hasMany: sandbox.spy()
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub
+  })
+
   db = await setupDatabase(config)
 })
 
+test.afterEach(() => {
+  sandbox && sinon.restore()
+})
+
 test('Agent', t => {
-  t.truthy(db.Agent, 'Agent service shoulld exist')
+  t.truthy(db.Agent, 'Agent service should exist')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricStub.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
 })
