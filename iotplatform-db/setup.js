38aa14b4432c4dcbe3add146aa5f0b58e109b5e1
Carlos Azaustre <cazaustre@gmail.com>
2018-11-29 16:04:50 +0100

    [db] setup db script

--- iotplatform-db/setup.js
+++ iotplatform-db/setup.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const debug = require('debug')('iotplatform:db:setup')
 const db = require('./')
 
 async function setup () {
@@ -9,10 +10,19 @@ async function setup () {
     password: process.env.DB_PASS || 'iotplatform',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
+    logging: s => debug(s),
     setup: true
   }
 
-  await db(config)
+  await db(config).catch(handleFatalError)
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.error(err.message)
+  console.error(err.stack)
+  process.exit(1)
 }
 
 setup()
