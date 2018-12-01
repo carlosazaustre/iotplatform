7db2160ad057fbb8c699ad1643597007860cb629
Carlos Azaustre <cazaustre@gmail.com>
2018-12-01 17:54:22 +0100

    Agent#findById

--- iotplatform-db/tests/agent.test.js
+++ iotplatform-db/tests/agent.test.js
@@ -2,6 +2,7 @@
 const proxyquire = require('proxyquire')
 const sinon = require('sinon')
 const test = require('ava')
+const agentFixtures = require('./fixtures/agent')
 
 let config = {
   logging: function () {}
@@ -11,6 +12,9 @@ let MetricStub = {
   belongsTo: sinon.spy()
 }
 
+let id = 1
+let uuid = 'yyy-yyy-yyy'
+let single = Object.assign({}, agentFixtures.single)
 let AgentStub = null
 let db = null
 let sandbox = null
@@ -18,8 +22,11 @@ let sandbox = null
 test.beforeEach(async () => {
   sandbox = sinon.createSandbox()
   AgentStub = {
-    hasMany: sandbox.spy()
+    hasMany: sandbox.spy(),
+    findById: sandbox.stub()
   }
+
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
 
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
@@ -42,4 +49,14 @@ test.serial('Setup', t => {
   t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
   t.true(MetricStub.belongsTo.called, 'MetricStub.belongsTo was executed')
   t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+})
+
+test.serial('Agent#findById', async t => {
+  let agent = await db.Agent.findById(id)
+
+  t.true(AgentStub.findById.called, 'findByUId should be called on model')
+  t.true(AgentStub.findById.calledOnce, 'findById should be called once')
+  t.true(AgentStub.findById.calledWith(id), 'findById should be called with args')
+
+  t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
 })
