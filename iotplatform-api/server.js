804f4172c16eefada7b390a7c55305c7deccdaf9
Carlos Azaustre <cazaustre@gmail.com>
2018-12-05 08:59:07 +0100

    [api] define routes

--- iotplatform-api/server.js
+++ iotplatform-api/server.js
@@ -4,9 +4,13 @@ const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
 
+const api = require('./api')
+
 const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
+
+app.use('/api', api)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[iotplatform-api]')} server listening on port ${port}`)
