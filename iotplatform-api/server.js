4f1d33f48eccbd2429752199dbcbeb3c2d657967
Carlos Azaustre <cazaustre@gmail.com>
2018-12-05 08:32:25 +0100

    [api] express app

---
+++ iotplatform-api/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const http = require('http')
+const express = require('express')
+const chalk = require('chalk')
+
+const port = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+
+server.listen(port, () => {
+  console.log(`${chalk.green('[iotplatform-api]')} server listening on port ${port}`)
+})
