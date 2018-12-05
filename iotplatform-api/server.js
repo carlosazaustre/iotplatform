69d7a097818a7c496dc41459b0a6ad38deb25dad
Carlos Azaustre <cazaustre@gmail.com>
2018-12-05 09:11:41 +0100

    [api] error handlers

--- iotplatform-api/server.js
+++ iotplatform-api/server.js
@@ -1,8 +1,10 @@
 'use strict'
 
+const debug = require('debug')('iotplatform:api')
 const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
+const { handleFatalError } = require('../errors')
 
 const api = require('./api')
 
@@ -11,6 +13,20 @@ const app = express()
 const server = http.createServer(app)
 
 app.use('/api', api)
+
+// Express Error Handler
+app.use((err, req, res, next) => {
+  debug(`Error: ${err.message}`)
+
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: err.message })
+})
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[iotplatform-api]')} server listening on port ${port}`)
