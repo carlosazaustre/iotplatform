1f27bc898d89647de8d214be611adf5363470fe1
Carlos Azaustre <cazaustre@gmail.com>
2018-12-09 18:18:58 +0100

    [web] add socket.io

--- iotplatform-web/server.js
+++ iotplatform-web/server.js
@@ -4,14 +4,29 @@ const debug = require('debug')('iotplatform:web')
 const http = require('http')
 const path = require('path')
 const express = require('express')
+const socketio = require('socket.io')
 const chalk = require('chalk')
 const { handleFatalError } = require('../errors')
 
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
+const io = socketio(server)
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+// Socket.io / Websockets
+io.on('connect', socket => {
+  debug(`Connected ${socket.id}`)
+
+  socket.on('agent/message', payload => {
+    console.log(payload)
+  })
+
+  setInterval(() => {
+    socket.emit('agent/message', { agent: 'xxx-yyy' })
+  }, 2000)
+})
 
 server.listen(port, () => {
   console.log(`${chalk.green('[iotplatform-web]')} server listening on port ${port}`)
