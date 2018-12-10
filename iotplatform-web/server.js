c9d78646195b1a6e707b127a397463af56a7e77f
Carlos Azaustre <cazaustre@gmail.com>
2018-12-10 09:21:29 +0100

    [web] integrate agent with socket.io

--- iotplatform-web/server.js
+++ iotplatform-web/server.js
@@ -6,12 +6,15 @@ const path = require('path')
 const express = require('express')
 const socketio = require('socket.io')
 const chalk = require('chalk')
+const IotPlatformAgent = require('iotplatform-agent')
 const { handleFatalError } = require('../errors')
+const { pipe } = require('./utils')
 
 const port = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+const agent = new IotPlatformAgent()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -19,17 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 io.on('connect', socket => {
   debug(`Connected ${socket.id}`)
 
-  socket.on('agent/message', payload => {
-    console.log(payload)
-  })
-
-  setInterval(() => {
-    socket.emit('agent/message', { agent: 'xxx-yyy' })
-  }, 2000)
+  pipe(agent, socket)
 })
 
 server.listen(port, () => {
   console.log(`${chalk.green('[iotplatform-web]')} server listening on port ${port}`)
+  agent.connect()
 })
 
 process.on('uncaughtException', handleFatalError)
